ead20c325423d93141a026139c4912abc3763ad6
theEvilReaper <steffenwx@gmail.com>
2025-04-15 10:29:17 +0200

    Update repository section

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,73 +8,41 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    if (System.getenv("CI") != null) {
-        repositoriesMode = RepositoriesMode.PREFER_SETTINGS
-        repositories {
-            maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-            maven("https://repo.htl-md.schule/repository/Gitlab-Runner/")
-            maven {
-                val groupdId = 28 // Gitlab Group
-                val ciApiv4Url = System.getenv("CI_API_V4_URL")
-                url = uri("$ciApiv4Url/groups/$groupdId/-/packages/maven")
-                name = "GitLab"
-                credentials(HttpHeaderCredentials::class.java) {
-                    name = "Job-Token"
-                    value = System.getenv("CI_JOB_TOKEN")
+    repositories {
+        mavenCentral()
+        maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        maven("https://reposilite.worldseed.online/public")
+        maven {
+            name = "OneLiteFeatherRepository"
+            url = uri("https://repo.onelitefeather.dev/onelitefeather")
+            if (System.getenv("CI") != null) {
+                credentials {
+                    username = System.getenv("ONELITEFEATHER_MAVEN_USERNAME")
+                    password = System.getenv("ONELITEFEATHER_MAVEN_PASSWORD")
                 }
+            } else {
+                credentials(PasswordCredentials::class)
                 authentication {
-                    create<HttpHeaderAuthentication>("header")
+                    create<BasicAuthentication>("basic")
                 }
             }
-        }
-    } else {
-        repositories {
-            maven {
-                val groupdId = 4 // Gitlab Group
-                url = uri("https://gitlab.onelitefeather.dev/api/v4/groups/$groupdId/-/packages/maven")
-                name = "GitLab"
-                credentials(HttpHeaderCredentials::class.java) {
-                    name = "Private-Token"
-                    value = providers.gradleProperty("gitLabPrivateToken").get()
-                }
-                authentication {
-                    create<HttpHeaderAuthentication>("header")
-                }
-            }
-            maven {
-                val groupdId = 28 // Gitlab Group
-                url = uri("https://gitlab.onelitefeather.dev/api/v4/groups/$groupdId/-/packages/maven")
-                name = "GitLab"
-                credentials(HttpHeaderCredentials::class.java) {
-                    name = "Private-Token"
-                    value = providers.gradleProperty("gitLabPrivateToken").get()
-                }
-                authentication {
-                    create<HttpHeaderAuthentication>("header")
-                }
-            }
-            maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-            mavenCentral()
-            maven("https://jitpack.io")
-            maven("https://reposilite.worldseed.online/public")
         }
     }
 
     versionCatalogs {
         create("libs") {
             version("microtus","1.5.0")
-            version("bom", "1.1.1")
+            version("bom", "1.1.2")
             version("publishdata", "1.4.0")
             version("shadow", "8.3.5")
 
             library("microtus.bom", "net.onelitefeather.microtus", "bom").versionRef("microtus")
-            library("dungeon.base.bom", "net.theevilreaper.dungeon.bom", "base").versionRef("bom")
+            library("mycelium.bom", "net.theevilreaper.mycelium.bom", "mycelium-bom").versionRef("bom")
+
             library("minestom", "net.onelitefeather.microtus", "Microtus").withoutVersion()
             library("minestom-test", "net.onelitefeather.microtus.testing", "testing").withoutVersion()
             library("junit.api", "org.junit.jupiter", "junit-jupiter-api").withoutVersion()
             library("junit.engine", "org.junit.jupiter", "junit-jupiter-engine").withoutVersion()
-            library("mockito.core", "org.mockito", "mockito-core").withoutVersion()
-            library("mockito.junit", "org.mockito", "mockito-junit-jupiter").withoutVersion()
             library("worldSeed", "net.worldseed.multipart", "WorldSeedEntityEngine").version("8.0.2")
 
             plugin("publishdata", "de.chojo.publishdata").versionRef("publishdata")
