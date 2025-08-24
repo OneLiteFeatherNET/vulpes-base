c35da3da923ff1c98461880ac059751a7aaeb907
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-08-24 17:45:08 +0200

    Update variable order

--- src/main/java/net/theevilreaper/vulpes/font/BitFontSymbol.java
+++ src/main/java/net/theevilreaper/vulpes/font/BitFontSymbol.java
@@ -1,15 +1,18 @@
 package net.theevilreaper.vulpes.font;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.util.Codec;
+import net.theevilreaper.vulpes.gson.KeyGsonAdapter;
 import net.theevilreaper.vulpes.registries.Registry;
 import net.theevilreaper.vulpes.registries.RegistryFactory;
 import net.theevilreaper.vulpes.registries.RegistryResources;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Unmodifiable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,27 +22,39 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Objects;
 
-public record BitFontSymbol(@NotNull Key key, int ascent, int height, @NotNull List<String> symbols) implements FontSymbol {
+public record BitFontSymbol(
+        @NotNull Key key,
+        int ascent,
+        int height,
+        @NotNull List<String> symbols
+) implements FontSymbol {
+
+    private static final Gson GSON = new GsonBuilder()
+            .registerTypeAdapter(Key.class, KeyGsonAdapter.createMinestom())
+            .create();
+    private static final Logger LOGGER = LoggerFactory.getLogger(BitFontSymbol.class);
+    private static final Codec<BitFontSymbol, JsonObject, Throwable, Throwable> BIT_FONT_SYMBOL_CODEC = new BitFontSymbol.BitFontSymbolCodec();
 
     public static final Registry<BitFontSymbol> REGISTRY = RegistryFactory.createRegistry(Key.key("vulpes", "fonts"), BitFontSymbol::loadEntries, RegistryResources.BITMAP_FONTS);
-    private static Codec<BitFontSymbol, JsonObject, Throwable, Throwable> BIT_FONT_SYMBOL_CODEC = new BitFontSymbol.BitFontSymbolCodec();
-    private static final Logger LOGGER = LoggerFactory.getLogger(BitFontSymbol.class);
-    private static final Gson GSON = new Gson();
 
     static BitFontSymbol get(Key key) {
         return REGISTRY.get(key);
     }
 
-    private static List<BitFontSymbol> loadEntries(InputStream inputStream) {
+    private static @NotNull @Unmodifiable List<BitFontSymbol> loadEntries(@NotNull InputStream inputStream) {
         JsonArray jsonElements = GSON.fromJson(new InputStreamReader(inputStream, StandardCharsets.UTF_8), JsonArray.class);
-        return jsonElements.asList().stream().map(JsonElement::getAsJsonObject).map(jsonObject -> {
-            try {
-                return BIT_FONT_SYMBOL_CODEC.decode(jsonObject);
-            } catch (Throwable e) {
-                LOGGER.error("Failed to decode font symbol: {}", jsonObject, e);
-                return null;
-            }
-        }).filter(Objects::nonNull).toList();
+        return jsonElements.asList().stream()
+                .map(JsonElement::getAsJsonObject)
+                .map(jsonObject -> {
+                    try {
+                        return BIT_FONT_SYMBOL_CODEC.decode(jsonObject);
+                    } catch (Throwable e) {
+                        LOGGER.error("Failed to decode font symbol: {}", jsonObject, e);
+                        return null;
+                    }
+                })
+                .filter(Objects::nonNull)
+                .toList();
     }
 
     static class BitFontSymbolCodec implements Codec<BitFontSymbol, JsonObject, Throwable, Throwable> {
@@ -54,5 +69,4 @@ public record BitFontSymbol(@NotNull Key key, int ascent, int height, @NotNull L
             return GSON.toJsonTree(decoded).getAsJsonObject();
         }
     }
-
 }
