e9020d2d5e28523c4171ab90e4a675715928029a
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-08-24 12:58:59 +0200

    Add some basic tests

---
+++ src/test/java/net/theevilreaper/vulpes/registries/ImmutableRegistryTest.java
@@ -0,0 +1,54 @@
+package net.theevilreaper.vulpes.registries;
+
+import net.kyori.adventure.key.Key;
+import net.theevilreaper.vulpes.registries.helper.StringKey;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ImmutableRegistryTest {
+
+    private static Key registryKey;
+    private static Registry<StringKey> staticRegistry;
+
+    @BeforeAll
+    static void setUp() {
+        registryKey = Key.key("vulpes", "test");
+        staticRegistry = RegistryFactory.createRegistry(registryKey, namespace -> {
+            List<StringKey> entries = new ArrayList<>();
+            entries.add(new StringKey(Key.key("vulpes", "test1")));
+            entries.add(new StringKey(Key.key("vulpes", "test2")));
+            return entries;
+        });
+
+        assertInstanceOf(ImmutableRegistry.class, staticRegistry, "The static registry should be an immutable registry");
+    }
+
+    @Test
+    void testBasicChecks() {
+        assertEquals(2, staticRegistry.size());
+        assertEquals(registryKey, staticRegistry.key());
+        assertEquals(staticRegistry.keys().size(), staticRegistry.values().size());
+    }
+
+    @Test
+    void testContains() {
+        assertTrue(staticRegistry.contains(new StringKey(Key.key("vulpes", "test1"))));
+        assertFalse(staticRegistry.contains(Key.key("vulpes", "test3")));
+    }
+
+    @Test
+    void testGet() {
+        var fetchedKey = staticRegistry.get(Key.key("vulpes", "test1"));
+        assertNotNull(fetchedKey);
+        assertInstanceOf(StringKey.class, fetchedKey);
+    }
+}
