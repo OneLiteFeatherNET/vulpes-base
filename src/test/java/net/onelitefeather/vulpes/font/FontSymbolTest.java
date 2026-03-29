6ef294a8aebf55d42634d26d1b814ccead7c0eab
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-03-29 10:39:25 +0200

    test(font): add new test cases

--- src/test/java/net/onelitefeather/vulpes/font/FontSymbolTest.java
+++ src/test/java/net/onelitefeather/vulpes/font/FontSymbolTest.java
@@ -2,7 +2,6 @@ package net.onelitefeather.vulpes.font;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import net.kyori.adventure.key.Key;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -24,6 +23,8 @@ class FontSymbolTest {
                     }
                     """;
 
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
     @Test
     void testNameTagRead() {
         FontSymbol tag = new FontSymbol(
@@ -34,8 +35,7 @@ class FontSymbolTest {
                 List.of("\uE120")
         );
 
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        FontSymbol loadedText = gson.fromJson(TEST_JSON, FontSymbol.class);
+        FontSymbol loadedText = GSON.fromJson(TEST_JSON, FontSymbol.class);
         assertNotNull(loadedText);
 
         assertEquals(tag.file(), loadedText.file());
@@ -44,5 +44,32 @@ class FontSymbolTest {
         assertEquals(tag.height(), loadedText.height());
         assertEquals(1,  tag.chars().size());
         assertEquals(tag.chars(), loadedText.chars());
+    }
+
+    @Test
+    void testNameTagMethod() {
+        FontSymbol loadedText = GSON.fromJson(TEST_JSON, FontSymbol.class);
+        assertNotNull(loadedText);
+
+        assertTrue(loadedText.hasChars());
+        assertEquals("\uE120", loadedText.getChar(0));
+        assertEquals("\uE120", loadedText.chars().getFirst());
+
+        assertNull(loadedText.getChar(1));
+        assertNull(loadedText.getChar(-1));
+        assertEquals("\uE000", loadedText.getCharOr(1, "\uE000"));
+        assertEquals("\uE000", loadedText.getCharOr(-1, "\uE000"));
+    }
+
+    @Test
+    void testEmptyChars() {
+        FontSymbol tag = new FontSymbol(
+                "manis:global/player_ranks/admin.png",
+                "bitmap",
+                10,
+                10,
+                List.of()
+        );
+        assertFalse(tag.hasChars());
     }
 }
